7b8d1922287862aa417668092eb19a507b80b4ce
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2019-10-14 19:15:18 +0600

    springboot project initiated

---
+++ src/main/java/com/nafisulbari/usis/UsisApplication.java
@@ -0,0 +1,13 @@
+package com.nafisulbari.usis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsisApplication.class, args);
+    }
+
+}
