24c3e974cc8dbd21764fa3e6ee93d396a6da3f9b
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2019-10-22 06:14:49 +0600

    Revert "Merge branch 'master' into new_master"
    This reverts commit f7a7fe5306d30f0d4062244dec371bcc3deb9c69.

--- src/main/java/com/nafisulbari/usis/service/PreviousPasswordServiceImpl.java
+++ src/main/java/com/nafisulbari/usis/service/PreviousPasswordServiceImpl.java
@@ -1,11 +1,15 @@
 package com.nafisulbari.usis.service;
 
 import com.nafisulbari.usis.entity.PasswordRequest;
+import com.nafisulbari.usis.entity.PreviousPassword;
+import com.nafisulbari.usis.entity.User;
 import com.nafisulbari.usis.security.MD5;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import java.util.List;
 
@@ -34,6 +38,8 @@ public class PreviousPasswordServiceImpl implements PreviousPasswordService {
                 return true;
             }
         }
+
         return false;
+
     }
 }
