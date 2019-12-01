1a75e170505146a4ec0af2544aab33289c47b152
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2019-12-01 23:29:05 +0600

    Clean set to work on clash resolver

--- src/main/java/com/nafisulbari/usis/controller/TeacherController.java
+++ src/main/java/com/nafisulbari/usis/controller/TeacherController.java
@@ -15,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -54,37 +55,35 @@ public class TeacherController {
         }
 
 //--------Student FOUND in db------------------------------------------------------------
-//--------Generate routine(A Course's list) and return student-panel---------------------
-        List<Advising> advisedCourses = advisingService.findAdvisedCourses(student.getId());
-        List<Course> routine = new ArrayList<>();
-        for (Advising advising : advisedCourses) {
-            routine.add(courseService.findCourseById(advising.getCourseId()));
-        }
+
         model.addAttribute("student", student);
-        model.addAttribute("routine", routine);
+        model.addAttribute("routine", getRoutine(student.getId()));
         return new ModelAndView("/teacher/student-panel");
     }
 
 
-//-------------on dev-------------------------------------------------------------------------------------------------------
-    @GetMapping("/teacher/student-panel")
-    public ModelAndView studentPanel(String courseCode, String courseType, @ModelAttribute("student") User student, Model model) {
+    //-------------on dev-------------------------------------------------------------------------------------------------------
+    @GetMapping("/teacher/student-panel/{id}")
+    public ModelAndView studentPanel(@PathVariable("id") int studentId, String courseCode, String courseType, Model model) {
 
+        User student = userService.findUserById(studentId);
+        System.out.println(student);
         System.out.println("CC: " + courseCode);
         System.out.println("CT:" + courseType);
 
-        System.out.println("model   :" + model);
-        System.out.println(student);
 
-//--------Generate routine(A Course's list) and return student-panel---------------------
-        List<Advising> advisedCourses = advisingService.findAdvisedCourses(student.getId());
+        model.addAttribute("student", student);
+        model.addAttribute("routine", getRoutine(studentId));
+        return new ModelAndView("/teacher/student-panel");
+    }
+
+//------Generates routine from studentId------------------------------------------------------------------------------------
+    private List<Course> getRoutine(int studentId) {
+        List<Advising> advisedCourses = advisingService.findAdvisedCourses(studentId);
         List<Course> routine = new ArrayList<>();
         for (Advising advising : advisedCourses) {
             routine.add(courseService.findCourseById(advising.getCourseId()));
         }
-        model.addAttribute("student", student);
-        model.addAttribute("routine", routine);
-        return new ModelAndView("/teacher/student-panel");
+        return routine;
     }
-
 }
