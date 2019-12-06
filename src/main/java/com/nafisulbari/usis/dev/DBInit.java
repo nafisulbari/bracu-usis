f6d2e8139f8e495d2a22b7d4e282849dab662b22
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2019-12-07 00:20:19 +0600

    course structure update

--- src/main/java/com/nafisulbari/usis/dev/DBInit.java
+++ src/main/java/com/nafisulbari/usis/dev/DBInit.java
@@ -1,6 +1,5 @@
 package com.nafisulbari.usis.dev;
 
-import com.nafisulbari.usis.entity.User;
 import com.nafisulbari.usis.repo.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,21 +19,21 @@ public class DBInit implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//-------------TO insert users during app launch
-//
-//        User std9 = new User();
-//        std9.setEmail("reg@bracu.ac.bd");
-//        std9.setPassword(passwordEncoder.encode("123"));
-//        std9.setActive(1);
-//        std9.setDept("");
-//        std9.setName("Reg");
-//        std9.setCredit("");
-//        std9.setMobile("0000");
-//        std9.setRole("ADMIN");
-//        std9.setPermissions("");
-//        std9.setRoutine("");
-//
-//        userRepository.save(std9);
+
+//------------RUNS EVERYTIME SERVER RUNS--------------------------------------------
+//        User user = new User();
+////        user.setEmail("std7@gmail.com");
+////        user.setPassword(passwordEncoder.encode("000"));
+////        user.setActive(1);
+////        user.setCourseLimit(3);
+////        user.setDept("CSE");
+////        user.setName("std7");
+////        user.setCredit("");
+////        user.setMobile("0000");
+////        user.setRole("STUDENT");
+////        user.setPermissions("");
+////        user.setRoutine("");
+////        userRepository.save(user);
 
 
     }
