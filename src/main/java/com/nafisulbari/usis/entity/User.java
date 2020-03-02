c25520760af6385fab109c6b78ba0a8b6679070a
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2020-03-03 01:37:48 +0600

    heroku-config

--- src/main/java/com/nafisulbari/usis/entity/User.java
+++ src/main/java/com/nafisulbari/usis/entity/User.java
@@ -1,16 +1,14 @@
 package com.nafisulbari.usis.entity;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 
 @Entity
+@Table(name = "user_entity")
 public class User {
 
     @Id
